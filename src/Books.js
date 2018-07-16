bfdc6d1eb71fbbea9cd19330cf0b1e28ac4f7cd2
zach-col <theperfectpicture100@gmail.com>
2018-07-16 17:19:06 -0500

    feat: create routes

--- src/Books.js
+++ src/Books.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 class Books extends Component {
 	render() {
 		return (
@@ -159,7 +159,10 @@ class Books extends Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            	<Link
+            	  to="/search"
+            	>Add a book</Link>
+
             </div>
           </div>
 		  // end of return
