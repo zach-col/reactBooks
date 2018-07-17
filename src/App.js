3eeebc4d68c1619bffa0cb908cb447ef6252f49e
zach-col <theperfectpicture100@gmail.com>
2018-07-17 13:13:04 -0500

    feat: get API data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import Search from './Search'
 import Books from './Books'
 import './App.css'
 
 class BooksApp extends React.Component {
-
+  state = {
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getALL().then((books) => {
+      this.setState( books )
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <Books/> 
+          <Books books={this.state.books}
+/> 
         )}/>
-        <Route exact path="/Search" component={Search}/>
+        <Route exact path="/Search" render={() => (
+          <Books /> 
+        )}/>
+
+
       </div>
     )
   }
