b26c9726af42bd4048e52bfb405421c7771d5180
zach-col <theperfectpicture100@gmail.com>
2018-07-18 18:33:01 -0500

    feat: add title/images to books

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class BooksApp extends React.Component {
   }
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
+      console.log(books)
       this.setState( { books } )
     })
   }
